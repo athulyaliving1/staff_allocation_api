459086766aaa86d0087022f4b899e97d10fad366
athulyaliving1 <muthukumar@athulyaliving.com>
2024-02-19 15:06:56 +0530

    Fix # db changed

--- db/dbconnection.js
+++ db/dbconnection.js
@@ -1,8 +1,9 @@
 const mysql = require('mysql2/promise');
 
 
-
 var config;
+
+
 config = {
   mysql_pool: mysql.createPool({
     host: "162.241.123.158",
@@ -10,9 +11,27 @@ config = {
     password: "r3pbWhs8psb5nitZjlpDvg",
     database: "theatgg6_sal_subscriber102",
     multipleStatements: true,
-    waitForConnections: true,
-    queueLimit: 0
   }),
+
+
+
+
+// config = {
+//   mysql_pool: mysql.createPool({
+//     host: "dbaas-db-10787912-do-user-15527658-0.c.db.ondigitalocean.com",
+//     user: "aaluser",
+//     password: "AVNS_fHFeMdTzckFxMa3MSZF",
+//     database: "theatgg6_sal_subscriber102",
+//     multipleStatements: true,
+//     waitForConnections: true,
+//     queueLimit: 0
+//   }),
+
+
+
+
+
+
   // mysql_pool : mysql.createPool({
   //     host     : '162.241.85.121',
   //     user     : 'athulslv_muthukumar',
